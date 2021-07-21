1886cb27c8044d83652129b32527ff64444c1840
mnaboti <71730801+mnaboti@users.noreply.github.com>
2021-07-21 22:08:47 +0200

    Update iblis_shippingx.py

--- iblis_shippingx.py
+++ iblis_shippingx.py
@@ -44,12 +44,13 @@ for site_id in cluster['site']:
         if subprocess.call(['ping', param, '1', site['ip_address']]) == 0:
 
             # ship data to remote site
-            push_iblis = "rsync " + "-r $WORKSPACE/iBLIS "+ site['username'] + "@" + site[
+            push_iblis = "rsync " + "-r $WORKSPACE/iBLIS.tar.gz, extract.sh "+ site['username'] + "@" + site[
                 'ip_address'] + ":/var/www/html/"
+            
             os.system(push_iblis)
             
              # Run setup script
-            run_iblis_script = "ssh " + site['username'] + "@" + site['ip_address'] + " 'cd /var/www/html/iBLIS && ./iblis_setup.sh'"
+            run_iblis_script = "ssh " + site['username'] + "@" + site['ip_address'] + " 'cd /var/www/html/ && ./extract.sh'+" 'cd iBLIS/ && ./iblis_setup.sh'"
             os.system(run_iblis_script)
 
             # send sms alert
