3f07f45cb3080a0f3bca0e5f702bb7f41104e664
mnaboti <71730801+mnaboti@users.noreply.github.com>
2021-07-21 13:11:38 +0200

    Update iblis_shippingx.py

--- iblis_shippingx.py
+++ iblis_shippingx.py
@@ -52,7 +52,6 @@ for site_id in cluster['site']:
             run_ssh = "ssh " + site['username'] + "@" + site['ip_address']
             os.system(run_ssh)
             
-            sudo -E chown $USER /var/www/html/var/lib/jenkins/workspace/lims-setup_master/iBLIS
             file = tarfile.open('iBLIS.tar.gz')
             file.extractall('D:/var/www/html')
             file.close()
