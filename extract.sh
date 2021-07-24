14adb3588d8ad241027afea8e7780ef4aae88f3b
mnaboti <71730801+mnaboti@users.noreply.github.com>
2021-07-24 07:57:03 +0200

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo letmein| sudo -S tar -xvf /var/www/html/iBLIS.tar.gz
+echo 123456| sudo -S tar -xvf /var/www/html/iBLIS.tar.gz
 
 sudo mv /var/www/html/var/lib/jenkins/workspace/lims-setup_master/iBLIS /var/www/html/iBLIS
 
