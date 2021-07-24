51b964d316e11019d7ada5a86796009b2b50ee61
mnaboti <71730801+mnaboti@users.noreply.github.com>
2021-07-24 08:49:04 +0200

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-echo 123456| sudo -S tar -xvf /var/www/html/iBLIS.tar.gz
+#echo 123456| sudo -S tar -xvf /var/www/html/iBLIS.tar.gz
 
-sudo mv /var/www/html/var/lib/jenkins/workspace/lims-setup_master/iBLIS /var/www/html/iBLIS
+#sudo mv /var/www/html/var/lib/jenkins/workspace/lims-setup_master/iBLIS /var/www/html/iBLIS
 
-sudo chmod -R 777 /var/www/html/iBLIS
+
+echo 123456| sudo -S chmod -R 777 /var/www/html/iBLIS
