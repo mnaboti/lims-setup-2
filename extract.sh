d19a48742e6cf45604e7346f414c3e92a03e8b41
mnaboti <71730801+mnaboti@users.noreply.github.com>
2021-07-28 04:10:42 +0200

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -2,7 +2,7 @@
 
 echo czemr2020!| sudo -S tar -xvf /var/www/html/iBLIS.tar.gz
 
-sudo mv /var/www/html/var/lib/jenkins/workspace/lims-setup_master/iBLIS /var/www/html/iBLIS
+#sudo mv /var/www/html/var/lib/jenkins/workspace/lims-setup_master/iBLIS /var/www/html/iBLIS
 
 
 echo czemr2020!| sudo -S chmod -R 777 /var/www/html/iBLIS
